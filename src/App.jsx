9aac1a2050cdf6eba8f54b349d16195199a36b08
walissonsou <walissonsouza96@hotmail.com>
2023-01-31 21:20:42 -0300

    feat: first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import NavBar from './components/NavBar/NavBar'
+import Header from './components/Header/Header'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+     <NavBar />
+     <Header />
+    </div>
+  )
+}
+
+export default App
