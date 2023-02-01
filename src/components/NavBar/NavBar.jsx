d68e2baf90e7987449b71592b367880bf6dc73b1
walissonsou <walissonsouza96@hotmail.com>
2023-02-01 15:48:07 -0300

    feat: update navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,37 +1,54 @@
-import React, { useState } from 'react'
-import logo1 from '../../data/images/logo1.png';
-import { Link } from 'react-scroll'
+import React, { useState } from "react";
+import logo1 from "../../data/images/logo1.png";
+import { Link } from "react-scroll";
 
 const NavBar = () => {
-  const [ nav, setNav] = useState(false)
+  const [nav, setNav] = useState(false);
   const changeBackground = () => {
-    if(window.scrollY >= 50){
-      setNav(true)
-    }
-    else{
+    if (window.scrollY >= 50) {
+      setNav(true);
+    } else {
       setNav(false);
     }
-  }
-  window.addEventListener('scroll', changeBackground)
+  };
+  window.addEventListener("scroll", changeBackground);
 
   return (
     <nav className={nav ? "nav active" : "nav"}>
-      <Link to='#' className='logo'>
-        <img src={logo1} alt='/' />
+      <Link to="/" className="logo">
+        <a href="#">
+          {" "}
+          <img src={logo1} alt="/" />{" "}
+        </a>
       </Link>
-      <input className='menu-btn' type='checkbox' id='menu-btn' />
-      <label className='menu-icon' htmlFor='menu-btn'>
-        <span className='nav-icon'></span>
+      <input className="menu-btn" type="checkbox" id="menu-btn" />
+      <label className="menu-icon" htmlFor="menu-btn">
+        <span className="nav-icon"></span>
       </label>
-      <ul className='menu'>
-        <li> <Link to='#'> Ínicio</Link></li>
-        <li> <Link to='#'> Ubicacion</Link></li>
-        <li> <Link to='#'> Profes</Link></li>
-        <li> <Link to='#'> La cancha </Link></li>
-        <li> <Link to='#'> Usteds </Link></li>
+      <ul className="menu">
+        <li>
+          {" "}
+          <Link to="#"> Ínicio</Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="#"> Ubicacion</Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="#"> Profes</Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="#"> La cancha </Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="#"> Usteds </Link>
+        </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
