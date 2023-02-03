aea561f36a1502d592e8629c8cfb186f5a24aa97
walissonsou <walissonsouza96@hotmail.com>
2023-02-03 13:36:55 -0300

    remove text

--- src/components/Offer/Offer.jsx
+++ src/components/Offer/Offer.jsx
@@ -4,9 +4,7 @@ export default function Offer() {
   return (
     <div id="presentaion">
       <div className="pr-heading">
-        <h1>
-          VEN Y ENTRENA<span> TU CUERPO</span> Y TU MENTE
-        </h1>
+        
         <p className="details"></p>
         <div className="pr-btns">
           <a href="#" className="pr-btn">
