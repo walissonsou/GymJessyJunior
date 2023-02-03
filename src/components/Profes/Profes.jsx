94a6d8886b76b21b3a6078d53fd1e56650bad0d3
walissonsou <walissonsouza96@hotmail.com>
2023-02-03 15:05:59 -0300

    feat: add new component with carousel

---
+++ src/components/Profes/Profes.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+import { motion } from "framer-motion";
+import profe1 from "../../data/images/profe1.jpg";
+import profe2 from "../../data/images/profe2.jpg";
+import profe3 from "../../data/images/profe3.jpg";
+import profe4 from "../../data/images/profe4.jpg";
+
+const images = [profe1, profe2, profe3, profe4];
+
+const Profes = () => {
+  const carousel = useRef();
+  const [width, setWidth] = useState(0);
+  useEffect(() => {
+    console.log(carousel.current);
+    setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth);
+  }, []);
+
+  return (
+    <div id="profes" className="container-slide">
+      <h1> STAFF </h1>
+
+      <div className="slide-container">
+        <motion.div
+          ref={carousel}
+          className="carousel"
+          whileTap={{ cursor: "grabbing" }}
+        >
+          <motion.div
+            className="inner"
+            drag="x"
+            dragConstraints={{ right: 0, left: -width }}
+            initial={{x: 500}}
+            animate={{x:0}}
+            transition={{ duration: 1}}
+          >
+            {images.map((image, index) => (
+              <motion.div key={index} className="item">
+                <img src={image} alt="Texto alt" />
+              </motion.div>
+            ))}
+          </motion.div>{" "}
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+export default Profes;
