c5d6c2a11541c10b6f6612518cd0c43917355698
walissonsou <walissonsouza96@hotmail.com>
2023-02-04 14:25:18 -0300

    feat: remove string

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <div id="about">
      
       <div className="about-text">
-        <h1> CONOCENOS AHORA! </h1>
+        <h1> CONOCENOS </h1>
         <p>
           {" "}
           Determinación y enfoque, este es el resultado de una mente sana. Pon una pizca de movimiento en tu vida y haz ejercicio. La fuerza no siempre gana, se necesita resiliencia. Coloca con cuidado la arandela sobre la barra porque hará tus sueños realidad.{" "}
