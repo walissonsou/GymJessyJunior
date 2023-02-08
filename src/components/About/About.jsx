91fd42643a62579217a21e399a0851824924f90e
walissonsou <walissonsouza96@hotmail.com>
2023-02-08 13:27:12 -0300

    feat: format document

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,12 +3,14 @@ import aboutimage from "../../data/images/about.png";
 const About = () => {
   return (
     <div id="about">
-     
       <div className="about-text">
         <h1> CONOCENOS </h1>
         <p>
           {" "}
-          Determinación y enfoque, este es el resultado de una mente sana. Pon una pizca de movimiento en tu vida y haz ejercicio. La fuerza no siempre gana, se necesita resiliencia. Coloca con cuidado la arandela sobre la barra porque hará tus sueños realidad.{" "}
+          Determinación y enfoque, este es el resultado de una mente sana. Pon
+          una pizca de movimiento en tu vida y haz ejercicio. La fuerza no
+          siempre gana, se necesita resiliencia. Coloca con cuidado la arandela
+          sobre la barra porque hará tus sueños realidad.{" "}
         </p>
         <div className="pr-btns">
           <a href="#" className="pr-btn">
