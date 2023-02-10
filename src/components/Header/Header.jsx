dcd85e76ecc9f948786c3fa463fbbb656e78f98d
walissonsou <walissonsouza96@hotmail.com>
2023-02-10 15:06:54 -0300

    feat: create Modal

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,26 @@
-import React from "react";
-import Button from '../Button/Button'
+import { useState, React } from "react";
+import Button from "../Button/Button";
+import Modal from "../Modal/modal";
+
 const Header = () => {
+
+  const [openModal, setOpenModal] = useState(false);
+  
   return (
-    <div id='main'>
-      <div className='name'>
+    <div id="main">
+      <div className="name">
         <h2> SIN EXCUSAS </h2>
         <h1>
           {" "}
-          <span>EL ÚNICO LÍMITE ERES</span> <br /> TU 
+          <span>EL ÚNICO LÍMITE ERES</span> <br /> TU
         </h1>
-        <p className='details'> MÁS QUE UN GIMNASIO, UNA FAMÍLIA </p>
-        
-        <Button title={'SUBSCRIBIRSE'} href={'http://wa.me/message/AGRVNKUMDIHAC1'} />
+        <p className="details"> MÁS QUE UN GIMNASIO, UNA FAMÍLIA </p>
+        <Button title={"SUBSCRIBIRSE"} onClick={() => setOpenModal(!openModal)} />
       </div>
+      <Modal isOpen={openModal} setOpenModal={() => setOpenModal(!openModal)}>
+         
+
+      </Modal>
     </div>
   );
 };
---
+++ src/components/Modal/modal.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+
+const BACKGROUND_STYLE = {
+  position: 'absolute',
+  top: "0",
+  bottom: "0",
+  left: "0",
+  right: "0",
+  backgroundColor: "rgb(0,0,0,0.7)",
+  zIndex: "10000000",
+};
+const MODAL_STYLE = {
+  position: "fixed",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  padding: "50px",
+  backgroundColor: "#fff",
+  borderRadius: "10px",
+  color: "black",
+};
+
+const STYLE_BUTTON = {
+  position: "absolute",
+  display: "flex",
+  justifyContent: "flex-end",
+  alignItems: "",
+};
+
+export default function Modal({ isOpen, children, setOpenModal }) {
+  if (isOpen) {
+    return (
+      <div style={BACKGROUND_STYLE}>
+        <div style={MODAL_STYLE}>
+          <button onClick={setOpenModal} className="closed-button">
+            {" "}
+            X{" "}
+          </button>{" "}
+          <div style={STYLE_BUTTON}> </div>
+          <div>
+            <form className="form">
+              <label> NOMBRE *</label>
+              <input type="text" required />
+              <label> TELÉFONO * </label>
+              <input type="text" placeholder="123-45-678"/>
+              <labe> SEXO  </labe>
+              <select id="estado" name="estado">
+                <option value="SP">MAS</option>
+                <option value="RJ">FEM</option>
+              </select>
+              <label> SERVICIO DESEADO * </label>
+              <select id="estado" name="estado">
+                <option value="FUNCIONAL">FUNCIONAL</option>
+                <option value="FUNCIONAL">FUNCIONAL</option>{" "}
+                <option value="FUNCIONAL">FUNCIONAL</option>{" "}
+                <option value="FUNCIONAL">FUNCIONAL</option>{" "}
+                <option value="FUNCIONAL">FUNCIONAL</option>
+              </select>
+              <label> FECHA DE NACIMIENTO * </label>
+              <input type="date" />
+              <label> OBSERVACIÓN </label>
+              <input></input>
+              <button type="submit" className="header-bttn">
+                {" "}
+                ENVIAR{" "}
+              </button>
+            </form>
+          </div>
+        </div>
+        ;
+      </div>
+    );
+  }
+  return null;
+}
